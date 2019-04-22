8c3ecf219333d541840ff62f9159fff443e99447
Dan Smith <dansmith.2011@my.bristol.ac.uk>
2019-04-22 10:34:06 +0100

    Update server for deploy

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 These env variables must be set to connect to the DB. Typically done in a .env file
 
-MONGO_DB_USER
-MONGO_DB_PASSWORD
-MONGO_DB_URI
+- MONGO_DB_USER
+- MONGO_DB_PASSWORD
+- MONGO_DB_URI
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const reviews = require('./routes/api/reviews');
 const app = express();
@@ -20,8 +21,15 @@ mongoose
   })
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World!'));
 app.use('/api/reviews', reviews);
+
+// Serve static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
